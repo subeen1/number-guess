f6b6cd334d83c2579e99b3b52d57390f35afa060
(장수빈) <juning135@gmail.com>
2024-07-01 13:04:09 +0900

    last commit

--- main.js
+++ main.js
@@ -12,8 +12,19 @@ let computerNum = 0; //여기서 const를 사용하면 에러가 뜬다.
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
-playButton.addEventListener("click", play);
+let resetButton = document.getElementById("reset-button");
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById("chance-area");
+let history = [];
 
+playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+//내가 입력한 숫자값이 리셋이된다. 구지 함수이름을 선언하지 않아도 될 느낌 (선언하면 메모리차지됨)
+// 이 함수가 다른데 사용하지 않을 때 사용해야한다.
+userInput.addEventListener("focus", function () {
+  userInput.value = "";
+});
 //Math.random() 0 ~1 사이 숫자를 반환
 function pickRandomNum() {
   computerNum = Math.floor(Math.random() * 100) + 1; //우리가 원하는 숫자의 범위는 1~ 100 까지의 숫자이다.
@@ -23,13 +34,47 @@ function pickRandomNum() {
 //함수도 매개변수로 넘길 수 있다.
 function play() {
   let userValue = userInput.value;
+  //유효성검사
+  if (userValue < 1 || userValue > 100) {
+    resultArea.textContent = "1과 100사이 숫자를 입력해주세요";
+    return;
+  }
+  //데이터 유효성 검사
+  if (history.includes(userValue)) {
+    resultArea.textContent =
+      "이미 입력한 숫자입니다. 다른 숫자를 입력해 주세요.";
+    return;
+  }
+  chances--;
+  chanceArea.textContent = `남은기회:${chances}`;
+  console.log("chance", chances);
   if (userValue < computerNum) {
     resultArea.textContent = "UP!!!";
   } else if (userValue > computerNum) {
     resultArea.textContent = "Down!!";
   } else {
     resultArea.textContent = "정답입니다!!";
+    gameOver = true;
   }
+  history.push(userValue);
+  console.log(history);
+
+  if (chances < 1) {
+    gameOver = true;
+  }
+  //gameover만 입력하면 reset 버튼을 누루고난 후에 남은기회 reset이 되지않는다.
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
+}
+
+function reset() {
+  //user input창이 깨끗하게 정리되고
+  userInput.value = "";
+  //새로운 번호가 생성되고
+  pickRandomNum();
+
+  resultArea.textContent = "결과값이 여기 나옵니다.";
 }
 
 pickRandomNum();
